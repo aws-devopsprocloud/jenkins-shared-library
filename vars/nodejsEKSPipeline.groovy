a3e2e3c260e5b6377ad3c6c5320caf27bcf5c573
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-08-25 09:42:13 +0530

    jenkins-shared-library

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -102,7 +102,7 @@ def call (Map configMap){
                                 sh '''
                                     set -e
 
-                                    REPO="${org}/roboshop-project/${component}"
+                                    REPO="${org}/roboshop-project"
 
                                     curl -s -L \
                                     -H "Accept: application/vnd.github+json" \
