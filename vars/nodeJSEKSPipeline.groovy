4b0a0edbed418345ea91bc0d851eb6de91f7cd4b
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-08-25 08:59:39 +0530

    jenkins-shared-library

--- vars/nodeJSEKSPipeline.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map configMap) {
-    echo "======================================"
-    echo "nodejsEKSPipeline shared library loaded"
-    echo "======================================"
-
-    echo "Project   : ${configMap.get('project')}"
-    echo "Component : ${configMap.get('component')}"
-}
