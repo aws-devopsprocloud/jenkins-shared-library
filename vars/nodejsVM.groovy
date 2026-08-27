007e9d1c99f425abb6ef4cfe62dfacaf5d0d5741
mac-devopsprocloud <premsagar.eri@devopsprocloud.in>
2026-08-27 18:21:12 +0530

    jenkins-shared-library

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -32,8 +32,8 @@ def call (Map configMap){
             steps {
                 sh """
                    sudo  dnf module disable nodejs -y
-                    sudo dnf module enable nodejs:20 -y
-                    sudo dnf install nodejs -y
+                   sudo dnf module enable nodejs:20 -y
+                   sudo dnf install nodejs -y
                 """
             }
         }
